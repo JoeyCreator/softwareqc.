febf6aa943d30640970fd70b4993bb38e1620021
Joey Cadieux <58401407+JoeyCreator@users.noreply.github.com>
2020-10-13 15:01:27 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="SoftwareQC" src="./assets/logo.png" />
+  <Home msg="SoftwareQC | A group of programmer in Quebec." />
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+export default {
+  name: "App",
+  components: {
+    Home: Home,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 40px;
+}
+</style>
